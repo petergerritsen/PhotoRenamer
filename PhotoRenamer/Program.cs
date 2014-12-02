271befabe32a6b46b9e914aeff2086618fd5d5ab
Peter Gerritsen <github@pjotr.info>
2014-12-02 08:38:32 +0100

    Update

--- PhotoRenamer/Program.cs
+++ PhotoRenamer/Program.cs
@@ -15,33 +15,34 @@ namespace PhotoRenamer {
 
             var di = new DirectoryInfo(directory);
             var files = di.GetFiles("*.jpg").Where(x => !x.Name.StartsWith(prefix));
+            //var files = di.GetFiles("Job_2002*.jpg");
             List<RenameFileInfo> filesToRename = new List<RenameFileInfo>();
             foreach (var fi in files) {
-                try {
-                    using (ExifReader reader = new ExifReader(fi.FullName)) {
-                        // Extract the tag data using the ExifTags enumeration
-                        DateTime datePictureTaken;
-                        if (reader.GetTagValue<DateTime>(ExifTags.DateTimeDigitized,
-                            out datePictureTaken)) {
-                            filesToRename.Add(new RenameFileInfo() {
-                                FullName = fi.FullName,
-                                PhotoTaken = datePictureTaken
-                            });
-                        }
-                        else {
-                            filesToRename.Add(new RenameFileInfo() {
-                                FullName = fi.FullName,
-                                PhotoTaken = fi.CreationTime
-                            });
-                        }
-                    }
-                }
-                catch (Exception ex) {
+                //try {
+                //    using (ExifReader reader = new ExifReader(fi.FullName)) {
+                //        // Extract the tag data using the ExifTags enumeration
+                //        DateTime datePictureTaken;
+                //        if (reader.GetTagValue<DateTime>(ExifTags.DateTimeOriginal,
+                //            out datePictureTaken)) {
+                //            filesToRename.Add(new RenameFileInfo() {
+                //                FullName = fi.FullName,
+                //                PhotoTaken = datePictureTaken
+                //            });
+                //        }
+                //        else {
+                //            filesToRename.Add(new RenameFileInfo() {
+                //                FullName = fi.FullName,
+                //                PhotoTaken = fi.CreationTime
+                //            });
+                //        }
+                //    }
+                //}
+                //catch (Exception ex) {
                     filesToRename.Add(new RenameFileInfo() {
                         FullName = fi.FullName,
                         PhotoTaken = fi.CreationTime
                     });
-                }
+                //}
             }
 
             foreach (var fileToRename in filesToRename) {
